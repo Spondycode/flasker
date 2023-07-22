1672f5e40392a9a59ebf54a4664c2e04cc02b145
Dave Allen <spondicious@me.com>
2023-07-22 21:43:52 +0200

    added the error pages

--- .null-ls_814785_hello.py
+++ .null-ls_814785_hello.py
@@ -9,25 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     stuff = "Whats going on here?"
-    fav_pizza = ["mushroom", "Four cheeses", "Vegetarian", "Vegan", 42]
+    fav_pizza = ["mushroom", "Four cheeses", "Vegetarian", "Vegan"]
     return render_template("index.html", stuff=stuff, fav_pizza=fav_pizza)
 
 
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name)
-
-
-# Create Custom Error Pages
-
-
-# Invalid url
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-
-# Internal Server Error
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template("500.html"), 500
