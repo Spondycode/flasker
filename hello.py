34cb369c765a8397f35baa82b5c0a6c98897624b
Dave Allen <spondicious@me.com>
2023-07-22 20:01:56 +0200

    back in business again

--- hello.py
+++ hello.py
@@ -8,4 +8,11 @@ app = Flask(__name__)
 # Create a route decorator
 @app.route("/")
 def index():
-    return "<h1>Hello Dude</h1>"
+    stuff = "Whats going on here?"
+    fav_pizza = ["mushroom", "Four cheeses", "Vegeetarian", "Vegan"]
+    return render_template("index.html", stuff=stuff, fav_pizza=fav_pizza)
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name=name)
