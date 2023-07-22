24e360a07ede7cec7bf3c1e5d7d370aef7e03988
Dave Allen <spondicious@me.com>
2023-07-22 15:16:59 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+
+## Create a Flask Instance
+app = Flask(__name__)
+
+
+# Create a route decorator
+@app.route("/")
+def index():
+    return "<h1>Hello Dude</h1>"
